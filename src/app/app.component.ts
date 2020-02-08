3cc7044e22879474dbea7d49b4a101b4c1ea8438
Nelson Foltz <nelsfoltz@gmail.com>
2020-02-08 13:55:03 -0500

    add: get request for search input and console.log results

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,8 @@ export class AppComponent {
   constructor(private wikipedia: WikipediaService) {}
 
   onTerm(term: string) {
-    const results = this.wikipedia.search(term);
-    console.log(results);
+    this.wikipedia.search(term).subscribe(response => {
+      console.log(response);
+    });
   }
 }
--- src/app/wikipedia.service.ts
+++ src/app/wikipedia.service.ts
@@ -8,7 +8,16 @@ export class WikipediaService {
   constructor(private http: HttpClient) {}
 
   search(term: string) {
-    return 'I am wikipedia search results';
+    return this.http.get('https://en.wikipedia.org/w/api.php', {
+      params: {
+        action: 'query',
+        format: 'json',
+        list: 'search',
+        utf8: '1',
+        srsearch: term,
+        origin: '*'
+      }
+    });
   }
 }
 
