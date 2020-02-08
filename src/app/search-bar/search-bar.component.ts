516c31f504ba4af3ac1bddffe7f34a1bae56d836
Nelson Foltz <nelsfoltz@gmail.com>
2020-02-08 10:11:29 -0500

    add: binding and event handler for input

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent implements OnInit {
+  term = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onInput(value: string) {
+    this.term = value;
   }
-
 }
