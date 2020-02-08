266c2ec5b93ebbe7327d7ba7cb1a4b73b2b6c1b3
Nelson Foltz <nelsfoltz@gmail.com>
2020-02-08 13:47:17 -0500

    add: httpclientmodule in app module and httpclient in service constructor

--- src/app/wikipedia.service.ts
+++ src/app/wikipedia.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WikipediaService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   search(term: string) {
     return 'I am wikipedia search results';
